ac1a926b4ad06753c464aa7624649f5594753cd2
Bashir <b_egeh@hotmail.com>
2020-04-05 16:27:32 -0400

    have proximate fish ids logged to console when bitefish is clicked

--- public/scripts/entities/bite_fish.js
+++ public/scripts/entities/bite_fish.js
@@ -2,5 +2,16 @@ class BiteFish extends Fish{
   constructor(options){
     super(options);
     this.imageUri = '/images/bite-fish.gif';
+    this.tank = options.tank;
+  }
+
+  checkProximity(){
+    let proximate_fishes = this.tank.getProximateDenizens(this.position, 5000).filter(denizen=> denizen.isTasty === true).map(fish => fish.id);
+    console.log(proximate_fishes);
+  }
+
+  onClick(){
+    this.checkProximity();
   }
 }
+
